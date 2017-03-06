20464705cf3956d0734fa4e7e2e9ed07c5b73563
FondFish <844875798@qq.com>
2017-03-06 19:01:34 +0800

    Update and rename symlib.h to type.h

---
+++ server/type.h
@@ -0,0 +1,35 @@
+
+#ifndef _TYPELIB_H
+#define _TYPELIB_H
+
+#define SUPPORT_64BIT
+
+#define VOID void 
+typedef void*               LPVOID;
+typedef unsigned long WORD32;
+typedef unsigned short      WORD16;
+typedef unsigned char   BYTE;
+typedef unsigned long  ULONG;
+typedef char                CHAR;
+typedef signed long         SWORD32;
+typedef signed long long    SWORD64;
+typedef BYTE    BOOLEAN;
+typedef signed long       LONG;
+typedef int       INT;
+typedef unsigned short  WORD;
+typedef unsigned char bool;
+typedef unsigned long long  WORD64;
+typedef unsigned short      UINT16;
+typedef unsigned char       UINT8;
+typedef unsigned int        UINT32;
+typedef unsigned long           SIZE_T;
+
+typedef int (*FUNCPTR)();
+
+#define true  1
+#define false 0
+
+#define SUCCESS  0
+#define ERROR_UNKNOWN  1
+
+#endif
